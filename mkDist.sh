81110876afac50afd505cda14972bb1ec42f1705
Ignacio Ureta <ignacio.ureta@live.com>
2017-06-13 12:10:41 -0400

    mkDist now properly working

--- mkDist.sh
+++ mkDist.sh
@@ -10,5 +10,5 @@ cp -r app/font-awesome dist
 cp app/notification.js dist/js
 cp app/pictures.js dist/js
 cp app/loadPicture.js dist/js
-sed -i 's/\/main-/main-/g; s/app/./g' dist/index.html
-sed -i -e 's/app\/img/img/g' main-*.min.js
+sed -i -e 's/\/main-/main-/g; s/app/./g' dist/index.html
+sed -i -e 's/app\/img/img/g' dist/main-*.min.js
